c5a235fd0001809c73d431e4de9cea5f0a5c958d
zhanggui <gui.zhang@100credit.com>
2017-10-31 15:28:46 +0800

    add Core Image Demo

---
+++ CoreImageDemo/CoreImageDemo/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  CoreImageDemo
+//
+//  Created by zhanggui on 2017/10/30.
+//  Copyright © 2017年 zhanggui. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
