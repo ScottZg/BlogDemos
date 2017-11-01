4e83d9271cbbede8567aff90fba8dfd3d9832bd9
zhanggui <gui.zhang@100credit.com>
2017-11-01 18:03:37 +0800

    no message

--- CoreImageDemo/CoreImageDemo/Native/Tool/FilterViewController.m
+++ CoreImageDemo/CoreImageDemo/Native/Tool/FilterViewController.m
@@ -14,6 +14,9 @@
 @property (weak, nonatomic) IBOutlet UISlider *slider;
 @property (weak, nonatomic) IBOutlet UIButton *compleButton;
 @property (nonatomic,strong)UIImagePickerController *picker;
+
+@property (nonatomic,strong)CIContext *context;
+@property (nonatomic,strong)CIFilter *filter;
 @end
 
 @implementation FilterViewController
@@ -26,21 +29,31 @@
     self.imageView.contentMode = UIViewContentModeCenter;
     [self addGest];
 }
+- (IBAction)sliderValueChangedAction:(UISlider *)sender {
+    CGFloat sliderValue = sender.value;
+    NSLog(@"%f",sliderValue);
+    [self.filter setValue:@(sliderValue) forKey:kCIInputIntensityKey];
+    
+   
+    CGImageRef cgimg = [self.context createCGImage:[self.filter outputImage] fromRect:[[self.filter outputImage] extent]];
+    UIImage *newImage = [UIImage imageWithCGImage:cgimg];
+    self.imageView.image = newImage;
+    
+    
+}
 - (IBAction)buttonAction:(id)sender {
     
     UIImage *image = self.imageView.image;
     CIImage *ciImage = [CIImage imageWithCGImage:image.CGImage];
     
-    
-    CIContext *context = [CIContext context];
-    
-    
-    CIFilter *filter = [CIFilter filterWithName:@"CISepiaTone"];
-    [filter setValue:ciImage forKey:kCIInputImageKey];
-    [filter setValue:@(self.slider.value) forKey:kCIInputIntensityKey];
+
     
     
-    CGImageRef cgimg = [context createCGImage:[filter outputImage] fromRect:[[filter outputImage] extent]];
+    [self.filter setValue:ciImage forKey:kCIInputImageKey];
+    [self.filter setValue:@(self.slider.value) forKey:kCIInputIntensityKey];
+    
+    
+    CGImageRef cgimg = [self.context createCGImage:[self.filter outputImage] fromRect:[[self.filter outputImage] extent]];
     UIImage *newImage = [UIImage imageWithCGImage:cgimg];
     CGImageRelease(cgimg);
     [self performSegueWithIdentifier:@"showGenerate" sender:newImage];
@@ -101,8 +114,12 @@
         UIImage *image = info[UIImagePickerControllerOriginalImage];
         self.imageView.image = image;
         self.imageView.contentMode = UIViewContentModeScaleAspectFit;
+        CIImage *ciImage = [CIImage imageWithCGImage:image.CGImage];
+        [self.filter setValue:ciImage forKey:kCIInputImageKey];
+        
     }
     [self dismissViewControllerAnimated:YES completion:nil];
+   
     
 }
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
@@ -119,5 +136,16 @@
     // Pass the selected object to the new view controller.
 }
 */
-
+- (CIContext *)context {
+    if (!_context) {
+        _context = [CIContext contextWithOptions:nil];
+    }
+    return _context;
+}
+- (CIFilter *)filter {
+    if (!_filter) {
+        _filter = [CIFilter filterWithName:@"CISepiaTone"];
+    }
+    return _filter;
+}
 @end
